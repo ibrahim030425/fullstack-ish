5a4e14e40fec969c2c679f335a2d4b708873d569
Ibrahim Erfatpoor <ibrahim_feily@hotmail.com>
2025-10-21 15:35:35 +0200

    added latest changes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,34 @@
-const express = require('express'),
-  path = require('path')
+const express = require("express"),
+  path = require("path");
 
-const app = express()
+const app = express();
 
-app.get('/api', (_request, response) => {
-  response.send({ hello: 'World' })
-})
+app.get("/api", (_request, response) => {
+  response.send({ hello: "World" });
+});
 
-app.use(express.static(path.join(path.resolve(), 'dist')))
+app.use(express.static(path.join(path.resolve(), "dist")));
 
 app.listen(3000, () => {
-  console.log('Redo på http://localhost:3000/')
+  console.log("Redo på http://localhost:3000/");
+});
+
+const dotenv = require("dotenv"),
+  { Client } = require("pg");
+
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.PGURI
+})
+
+client.connect()
+
+app.get('/api', async (_request, response) => {
+  const { rows } = await client.query(
+    'SELECT * FROM cities WHERE name = $1',
+    ['Stockholm']
+  )
+
+  response.send(rows)
 })
